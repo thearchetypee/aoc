b6865ac576d5507550665c1ca81bda6cfba31747
aayush287 <aayushchaudhary287@gmail.com>
2024-12-11 11:09:54 +0530

    Day 11: Optimised solution using go routines

--- day11/main.go
+++ day11/main.go
@@ -2,55 +2,120 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/aoc2024/helper"
 )
 
-func processStone(stone int64) []int64 {
-	strStone := strconv.FormatInt(stone, 10)
+const WORKERS = 8
 
+type Stone struct {
+	value uint64
+	count uint64
+}
+
+type StoneMap struct {
+	mu   sync.RWMutex
+	data map[uint64]uint64
+}
+
+func NewStoneMap() *StoneMap {
+	return &StoneMap{
+		data: make(map[uint64]uint64),
+	}
+}
+
+func (sm *StoneMap) Add(value uint64, count uint64) {
+	sm.mu.Lock()
+	sm.data[value] += count
+	sm.mu.Unlock()
+}
+
+func (sm *StoneMap) GetAndClear() map[uint64]uint64 {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	result := sm.data
+	sm.data = make(map[uint64]uint64)
+	return result
+}
+
+func processStone(stone uint64) []Stone {
 	if stone == 0 {
-		return []int64{1}
+		return []Stone{{value: 1, count: 1}}
 	}
 
+	strStone := strconv.FormatUint(stone, 10)
 	if len(strStone)%2 == 0 {
 		mid := len(strStone) / 2
 		leftStr := strStone[:mid]
 		rightStr := strStone[mid:]
 
-		left, _ := strconv.ParseInt(leftStr, 10, 64)
-		right, _ := strconv.ParseInt(rightStr, 10, 64)
-		return []int64{left, right}
+		left, _ := strconv.ParseUint(leftStr, 10, 64)
+		right, _ := strconv.ParseUint(rightStr, 10, 64)
+		return []Stone{{value: left, count: 1}, {value: right, count: 1}}
 	}
 
-	return []int64{stone * 2024}
+	return []Stone{{value: stone * 2024, count: 1}}
 }
 
-func blink(stones []int64) []int64 {
-	var result []int64
+func worker(jobs <-chan Stone, results *StoneMap, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for stone := range jobs {
+		newStones := processStone(stone.value)
+		for _, ns := range newStones {
+			results.Add(ns.value, ns.count*stone.count)
+		}
+	}
+}
 
-	for _, stone := range stones {
-		result = append(result, processStone(stone)...)
+func blink(stones map[uint64]uint64) map[uint64]uint64 {
+	jobs := make(chan Stone, len(stones))
+	results := NewStoneMap()
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKERS; i++ {
+		wg.Add(1)
+		go worker(jobs, results, &wg)
 	}
 
-	return result
+	for value, count := range stones {
+		jobs <- Stone{value: value, count: count}
+	}
+	close(jobs)
+
+	wg.Wait()
+
+	return results.GetAndClear()
 }
 
-func solve(input []string) (int, int) {
-	var stones []int64
-
+func solve(input []string) (uint64, uint64) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	currentStones := make(map[uint64]uint64)
 	for _, numStr := range strings.Fields(input[0]) {
-		num, _ := strconv.ParseInt(numStr, 10, 64)
-		stones = append(stones, num)
+		num, _ := strconv.ParseUint(numStr, 10, 64)
+		currentStones[num]++
 	}
 
-	for i := 0; i < 25; i++ {
-		stones = blink(stones)
+	for i := 0; i < 75; i++ {
+		currentStones = blink(currentStones)
+		if (i+1)%10 == 0 {
+			var total uint64
+			for _, count := range currentStones {
+				total += count
+			}
+			fmt.Printf("After %d blinks: %d stones (unique: %d)\n",
+				i+1, total, len(currentStones))
+		}
 	}
 
-	return len(stones), 0
+	var total uint64
+	for _, count := range currentStones {
+		total += count
+	}
+	return total, 0
 }
 
 func main() {
